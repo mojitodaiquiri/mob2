891a20c841e52925d006026b32e4d2de103f0c65
mojitodaiquiri <mojitodaiquiri@gmail.com>
2013-09-13 10:25:47 +0200

    Revert "butto su tutto"
    This reverts commit 92c63954427c6083d7f5aa2d6f6b5f08743a4416.

--- js/personalizzazioni.js
+++
@@ -1,5 +0,0 @@
-$(document).bind("mobileinit", function(){
-	$.mobile.listview.prototype.options.filterPlaceholder= "Filtro ...";
-    $.mobile.page.prototype.options.backBtnText = "Indietro";
-});
-
